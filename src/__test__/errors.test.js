a5e30f7f475246211e317f8496a70d20e8dfc8df
Redstone Zhao <zhaoxiongfei@nequal.com>
2021-01-08 10:02:05 +0800

    增加额外功能

--- src/__test__/errors.test.js
+++ src/__test__/errors.test.js
@@ -21,4 +21,26 @@ describe("Errors", () => {
     expect(errors.key3("hello", "world").message).toBe("message3 %s");
     expect(errors.key3("hello", "world").data).toEqual(["hello", "world"]);
   });
+
+  it("case2", () => {
+    const defines = [
+      ["key1", "message1"],
+      ["key2", "message2 %d, %s"],
+      ["key3", "message3 %s"]
+    ];
+
+    const errors = Errors(defines);
+    expect(Object.keys(errors).length).toBe(3);
+    expect(errors.key1(errors.key2()).message).toBe("message2 %d, %s");
+    expect(errors.key2("hi").message).toBe("message2 %d, %s");
+    expect(errors.key2("hi").data).toEqual(["hi"]);
+    expect(errors.key3().message).toBe("message3 %s");
+
+    expect(errors.key2("20", "good").message).toBe("message2 %d, %s");
+    expect(errors.key2("20", "good").data).toEqual(["20", "good"]);
+
+    // world string be appened at the tail
+    expect(errors.key3("hello", "world").message).toBe("message3 %s");
+    expect(errors.key3("hello", "world").data).toEqual(["hello", "world"]);
+  });
 });
--- src/errors.js
+++ src/errors.js
@@ -7,6 +7,11 @@ function Errors(defines) {
   const errors = {};
   for (const [code, msg] of defines) {
     errors[code] = (...args) => {
+      if (Array.isArray(args) && args.length === 1) {
+        const [first] = args;
+        // 只有一个参数且，参数已经是一个封装后的
+        if (first instanceof Error && first.code) return first;
+      }
       const error = new Error(msg);
       error.code = code;
       error.data = args;
